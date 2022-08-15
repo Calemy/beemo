d1beba714c283930a1a1e289c99012e1657391b1
Nanoo <45440100+Calemy@users.noreply.github.com>
2022-08-15 16:07:03 +0200

    oopsie typo

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import api from "./modules/api.js"
-import { updateRanks, loadChannel } from "./modules/cron.js"
+import { updateRanks, loadChannels } from "./modules/cron.js"
 import database from "./helper/database.js"
 import logger from "./helper/logger.js"
 
@@ -8,7 +8,7 @@ async function main(){
     logger.purpleBlue("Connected to database").send()
     await updateRanks()
     logger.purpleBlue("Updated ranks").send()
-    await loadChannel()
+    await loadChannels()
     logger.purpleBlue("Loaded channels").send()
     logger.rainbow("Started Beemo v1.0.5").purpleBlue().send()
     api()
